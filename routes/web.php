cb2cc31abc4a5e93f778202ca4f933e13c4615d5
Marek Mikula <marek@justitpro.com>
2023-07-20 23:33:33 +0200

    feat: removed useless property

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    /** @var \App\Models\User $user */
-    $user = \App\Models\User::query()->first();
-
-    dd($user->mfaTokens);
-
     return view('welcome');
 });
