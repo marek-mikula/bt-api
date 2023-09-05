615bcaab38d416e3b658617605f338f212c46b5d
Marek Mikula <marek@justitpro.com>
2023-09-05 21:29:52 +0200

    feat: added mocks for binance

--- src/Domain/Binance/Http/Client/WalletClientMock.php
+++ src/Domain/Binance/Http/Client/WalletClientMock.php
@@ -5,26 +5,38 @@ namespace Domain\Binance\Http\Client;
 use Domain\Binance\Data\KeyPairData;
 use Domain\Binance\Http\Client\Concerns\WalletClientInterface;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Str;
 
 class WalletClientMock implements WalletClientInterface
 {
     public function systemStatus(): Response
     {
-        return response_from_client();
+        return response_from_client(data: $this->mockData('system-status.json'));
     }
 
     public function accountStatus(KeyPairData $keyPair): Response
     {
-        return response_from_client();
+        return response_from_client(data: $this->mockData('account-status.json'));
     }
 
     public function accountSnapshot(KeyPairData $keyPair): Response
     {
-        return response_from_client();
+        return response_from_client(data: $this->mockData('account-snapshot.json'));
     }
 
     public function assets(KeyPairData $keyPair): Response
     {
-        return response_from_client();
+        return response_from_client(data: $this->mockData('assets.json'));
+    }
+
+    private function mockData(string $path): array
+    {
+        $path = Str::startsWith($path, '/') ? Str::after($path, '/') : $path;
+
+        $json = file_get_contents(
+            filename: domain_path('Binance', "Resources/mocks/{$path}")
+        );
+
+        return json_decode(json: $json, associative: true);
     }
 }
