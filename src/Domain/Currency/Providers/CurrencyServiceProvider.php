edc5ff9376fa5a2ef018a125cb2362f182dc5219
Marek Mikula <marek@justitpro.com>
2023-09-12 22:51:09 +0200

    feat: schedule for syncing currencies

--- src/Domain/Currency/Providers/CurrencyServiceProvider.php
+++ src/Domain/Currency/Providers/CurrencyServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Domain\Currency\Providers;
 
+use Domain\Currency\Schedules\SyncCurrenciesSchedule;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\ServiceProvider;
 
 class CurrencyServiceProvider extends ServiceProvider
@@ -13,6 +15,15 @@ class CurrencyServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->bootSchedule();
+        }
+    }
+
+    private function bootSchedule(): void
+    {
+        $this->callAfterResolving(Schedule::class, static function (Schedule $schedule): void {
+            $schedule->call(SyncCurrenciesSchedule::proxyCall())->dailyAt('00:00');
+        });
     }
 }
