9f07a52c992c766c211250e50b3164955aea2b6e
Marek Mikula <marek@justitpro.com>
2023-10-04 13:27:13 +0200

    feat: mock files for news

--- src/Apis/Cryptopanic/Http/Client/CryptopanicClientMock.php
+++ src/Apis/Cryptopanic/Http/Client/CryptopanicClientMock.php
@@ -6,6 +6,7 @@ use Apis\Cryptopanic\Http\Client\Concerns\CryptopanicClientInterface;
 use App\Traits\MocksData;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class CryptopanicClientMock implements CryptopanicClientInterface
 {
@@ -13,6 +14,33 @@ class CryptopanicClientMock implements CryptopanicClientInterface
 
     public function latestNews(Collection $currencies = null): Response
     {
-        return response_from_client(data: $this->mockData('Cryptopanic', 'latest-news.json'));
+        if ($currencies === null) {
+            return response_from_client(data: $this->mockData('Cryptopanic', 'latest-news.json'));
+        }
+
+        // collect data for each given currency
+
+        // first load empty json
+        $data = $this->mockData('Cryptopanic', 'empty.json');
+
+        foreach ($currencies as $currency) {
+            $currency = Str::lower($currency);
+
+            // there is no mock file for this specific currency
+            // => skip
+            if (! file_exists(api_path('Cryptopanic', "Resources/mocks/latest-news-{$currency}.json"))) {
+                continue;
+            }
+
+            $currencyData = $this->mockData('Cryptopanic', "latest-news-{$currency}.json");
+
+            // merge currency data to the mail data variable
+            $data['results'] = array_merge(
+                array_values($data['results']),
+                array_values($currencyData['results'])
+            );
+        }
+
+        return response_from_client(data: $data);
     }
 }
