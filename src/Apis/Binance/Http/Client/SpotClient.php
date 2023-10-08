b2b8a756c690809b7812816c62c2c55ba2b04e6b
Marek Mikula <marek@justitpro.com>
2023-10-08 21:58:03 +0200

    feat: binance testnet config

--- src/Apis/Binance/Http/Client/SpotClient.php
+++ src/Apis/Binance/Http/Client/SpotClient.php
@@ -6,38 +6,21 @@ use Apis\Binance\Data\KeyPairData;
 use Apis\Binance\Exceptions\BinanceRequestException;
 use Apis\Binance\Http\BinanceResponse;
 use Apis\Binance\Http\Client\Concerns\SpotClientInterface;
-use Apis\Binance\Services\BinanceAuthenticator;
-use Illuminate\Http\Client\PendingRequest;
-use Illuminate\Support\Facades\Http;
 
-class SpotClient implements SpotClientInterface
+class SpotClient extends BinanceClient implements SpotClientInterface
 {
-    public function __construct(
-        private readonly BinanceAuthenticator $authenticator,
-    ) {
-    }
+    protected bool $supportsTestnet = true;
 
     public function account(KeyPairData $keyPair): BinanceResponse
     {
-        $params = $this->authenticator->sign($keyPair, []);
+        $params = $this->signParams($keyPair, []);
 
-        $response = $this->authRequest($keyPair)
-            ->get('/api/v3/account', $params);
+        $response = $this->authRequest($keyPair)->get('/api/v3/account', $params);
 
         if ($response->failed()) {
             throw new BinanceRequestException(new BinanceResponse($response));
         }
 
         return new BinanceResponse($response);
-    }
-
-    private function request(): PendingRequest
-    {
-        return Http::baseUrl((string) config('binance.url'));
-    }
-
-    private function authRequest(KeyPairData $keyPair): PendingRequest
-    {
-        return $this->authenticator->authenticate($keyPair, $this->request());
     }
 }
